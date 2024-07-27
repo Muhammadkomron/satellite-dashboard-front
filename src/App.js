50552f2fa0911692dc42ba338fbdde18639583dc
Nurmuxammad <108805831+lilnurik@users.noreply.github.com>
2024-07-27 12:48:11 +0500

    fixed-fullpage-fill

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Canvas} from '@react-three/fiber';
+import { Canvas } from '@react-three/fiber';
 import Satellite from './components/Satellite';
 import AltitudeGraph from './components/AltitudeGraph';
-import {WebSocketProvider} from './contexts/WebSocketProvider';
+import { WebSocketProvider } from './contexts/WebSocketProvider';
 import './App.css';
-import {ContactShadows} from "@react-three/drei";
+import { ContactShadows } from "@react-three/drei";
 import RealTimeData from "./components/RealTimeData";
 import AccelerationArrow from "./components/AccelerationArrow";
 import WebRTCPlayer from "./components/WebRTCPlayer";
@@ -16,27 +16,27 @@ function App() {
                 <div className="content">
                     <div className="data-cells">
                         <div className="cell">
-                            <RealTimeData/>
+                            <RealTimeData />
                         </div>
-                        <div className="graph">
-                            <AltitudeGraph/>
+                        <div className="cell graph">
+                            <AltitudeGraph />
                         </div>
                         <div className="video-cell">
-                            <WebRTCPlayer/>
+                            <WebRTCPlayer />
                         </div>
                     </div>
                     <div className="canvas-container">
                         <Canvas className="canvas">
-                            <ambientLight intensity={0.5}/>
-                            <directionalLight position={[10, 10, 5]}/>
-                            <Satellite/>
-                            <ContactShadows frames={1} position={[0, -3, 0]} blur={3} opacity={0.1}/>
-                            <ContactShadows frames={1} position={[0, -3, 0]} blur={6} opacity={0.2} color="red"/>
+                            <ambientLight intensity={0.5} />
+                            <directionalLight position={[10, 10, 5]} />
+                            <Satellite />
+                            <ContactShadows frames={1} position={[0, -3, 0]} blur={3} opacity={0.1} />
+                            <ContactShadows frames={1} position={[0, -3, 0]} blur={6} opacity={0.2} color="red" />
                         </Canvas>
                         <Canvas className="canvas">
-                            <ambientLight/>
-                            <pointLight position={[10, 8, 5]}/>
-                            <AccelerationArrow/>
+                            <ambientLight />
+                            <pointLight position={[10, 8, 5]} />
+                            <AccelerationArrow />
                         </Canvas>
                     </div>
                 </div>
