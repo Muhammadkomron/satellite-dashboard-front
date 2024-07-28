1c97a94be14a4a93aaafa7002c73df9b7741d448
Nurmuxammad <108805831+lilnurik@users.noreply.github.com>
2024-07-28 12:42:26 +0500

    fixing

--- src/components/Satellite.js
+++ src/components/Satellite.js
@@ -1,33 +1,43 @@
-import React, {useRef} from 'react';
-import {useFrame, useThree, useLoader} from '@react-three/fiber';
-import {useWebSocket} from '../contexts/WebSocketProvider';
-import * as THREE from "three/src/math/MathUtils";
-import {OrbitControls, Text} from "@react-three/drei";
-import {TextureLoader} from 'three';
+import React, { useRef } from 'react';
+import { useFrame, useThree, useLoader } from '@react-three/fiber';
+import { useWebSocket } from '../contexts/WebSocketProvider';
+import * as THREE from 'three';
+import { OrbitControls, Text } from '@react-three/drei';
+import { TextureLoader } from 'three';
 
 const Satellite = () => {
     const ref = useRef();
     const textRef = useRef();
-    const {data} = useWebSocket();
-    const {camera, gl} = useThree();
+    const { data } = useWebSocket();
+    const { camera, gl } = useThree();
 
-    // Load the texture for the logo
     const logoTexture = useLoader(TextureLoader, '/icons/NazarX New Black.png');
 
-    useFrame(() => {
+    const currentRotation = useRef(new THREE.Euler(0, 0, 0));
+    const targetRotation = new THREE.Euler(
+        THREE.MathUtils.degToRad(data.gyro.pitch),
+        THREE.MathUtils.degToRad(data.gyro.yaw),
+        THREE.MathUtils.degToRad(data.gyro.roll)
+    );
+
+    useFrame((state, delta) => {
         if (ref.current) {
-            ref.current.rotation.set(THREE.MathUtils.degToRad(data.gyro.pitch), THREE.MathUtils.degToRad(data.gyro.yaw), THREE.MathUtils.degToRad(data.gyro.roll));
+            // Smoothly interpolate the rotation towards the target rotation
+            currentRotation.current.x = THREE.MathUtils.lerp(currentRotation.current.x, targetRotation.x, 0.1);
+            currentRotation.current.y = THREE.MathUtils.lerp(currentRotation.current.y, targetRotation.y, 0.1);
+            currentRotation.current.z = THREE.MathUtils.lerp(currentRotation.current.z, targetRotation.z, 0.1);
+            ref.current.rotation.copy(currentRotation.current);
         }
         if (textRef.current) {
-            textRef.current.quaternion.copy(camera.quaternion); // Make sure the text always faces the camera
+            textRef.current.quaternion.copy(camera.quaternion);
         }
     });
 
     return (
         <>
-            <mesh ref={ref} scale={[0.5, 0.5, 0.5]} position={[0, 0, 0]}>
-                <cylinderGeometry args={[1, 1, 5, 32]}/>
-                <meshStandardMaterial map={logoTexture} roughness={0.7} metalness={0.3}/>
+            <mesh ref={ref} scale={[0.6, 0.6, 0.6]}>
+                <cylinderGeometry args={[1, 1, 5, 32]} />
+                <meshStandardMaterial map={logoTexture} roughness={0.7} metalness={0.3} />
             </mesh>
             <Text
                 ref={textRef}
@@ -35,13 +45,13 @@ const Satellite = () => {
                 color="black"
                 anchorX="left"
                 anchorY="top"
-                position={[-4, 3.5, 0]} // Adjust position to place the text in the corner
+                position={[-4, 3.5, 0]}
             >
                 {`pitch: ${(data.gyro.pitch || 0).toFixed(2)}, roll: ${(
                     data.gyro.roll || 0
                 ).toFixed(2)}, yaw: ${(data.gyro.yaw || 0).toFixed(2)}`}
             </Text>
-            <OrbitControls enableZoom={false} enablePan={false} enableRotate={false} args={[camera, gl.domElement]}/>
+            <OrbitControls enableZoom={false} enablePan={false} enableRotate={false} args={[camera, gl.domElement]} />
         </>
     );
 };
